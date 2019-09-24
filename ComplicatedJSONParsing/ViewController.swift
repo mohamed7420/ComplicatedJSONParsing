b3842a29493ee7e18d2d8fb14805a6e210ab4838
mohamedosama2074 <iosengks74@gmail.com>
2019-09-24 22:27:39 +0200

    Complecated JSON parsing

--- ComplicatedJSONParsing/ViewController.swift
+++ ComplicatedJSONParsing/ViewController.swift
@@ -12,9 +12,47 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+            let data = """
+            
+            {
+                "places":[
+                
+                {
+                "name" : "Mohamed",
+                "email" : "mo7med9878@gmail.com"
+                "Age" : 22
+                
+                },
+                {
+                
+                "name" : "Mohamed",
+                "email" : "mo7med9878@gmail.com",
+                "Age" : 22
+                
+                }
+                
+                ]
+                
+        }
+""".data(using: .utf8)
+    
+        let decode = try! JSONDecoder().decode([String:[User]].self, from: data!)
+        
+        let resylt = decode["places"]
+        print(resylt)
+        
     }
 
 
+}
+
+
+struct User : Decodable{
+    
+    var name:String
+    var email:String
+    var Age:Int
+    
 }
 
